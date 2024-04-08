08a978c8e7119ba759bfc2575c02ac0516353b6f
ChristianSteen-SW23 <cniel@student.aau.dk>
2024-04-08 15:49:30 +0200

    Made page for deck selctor
    Co-authored-by: ninaBlegmand <ninaBlegmand@users.noreply.github.com>

--- src/CreateDeckPage.jsx
+++ src/CreateDeckPage.jsx
@@ -17,12 +17,46 @@ function CreateDeckPage(){
     })*/
 
   //Renders list of decks on page
-  const deckArray = [{name:"hej"},{name:"hej2"},{name:"hej3"},{name:"hej4"},{name:"hej5"}]
+  const deckArray = [{name:"hej1", id: 0},{name:"hej2", id: 1},{name:"hej3", id: 2},{name:"hej4", id: 3},{name:"hej5", id: 4}]
 
   let [decks, setDecks]=useState(deckArray);
 
-  
-  
+  //add new deck
+  const addDeck = () => {
+    const newDeck = { name: "New Deck", id: decks.length + 1 };
+    const updatedDecks = [...decks, newDeck];
+    setDecks(updatedDecks);
+  };
+
+  //delete deck
+  const deleteDeck = (deleteIndex) => {
+    const updatedDecks = decks.filter((deck, index) => index !== deleteIndex);
+    setDecks(updatedDecks);
+  };
+
+  function DeckList(props){
+    return <>
+      <div className="list-group" id="list-tab" key="list">
+        {props.decks.map((deck) =>
+          <>
+            <div className="row">
+              <div className="col-10">
+                <button className="list-group-item list-group-item-action" id={"Deck:"+deck.id} onClick={()=>{console.log(deck.name)}}>
+                  {deck.name+deck.id}
+                </button>
+              </div>
+              <div className="col-2">
+                <button className="list-group-item list-group-item-action" id={"DelDeck:"+deck.id} onClick={()=>{deleteDeck(deck.id)}}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          </>)
+        }
+      </div>
+    </>
+  }
+
   return (
     <>
       {/* DeckPicker */}
@@ -36,14 +70,13 @@ function CreateDeckPage(){
         </div>
         <div className="row p-4"></div>
         <div className="row">
-          <button type="button" className="btn btn-primary" onClick={()=>{setDecks(()=>{[...decks,{name:"New Deck"}]}); console.log(decks)}}>Create New Deck</button>
+          {/*<button type="button" className="btn btn-primary" onClick={()=>{setDecks(()=>{[decks,{name:"New Deck", key: 6}]}); console.log(decks)}}>Create New Deck</button>*/}
+          <button type="button" className="btn btn-primary" onClick={()=>{addDeck()}}>Create New Deck</button>
         </div>
         <div className="row p-3"></div>
         <div className="row">
           <DeckList decks={decks} />
-        </div>
-        
-        
+        </div>        
       </div>
       
       {/* DeckEditor */}
@@ -56,30 +89,5 @@ function CreateDeckPage(){
     </>
   );
 }
-function DeckList(props){
-    const listDecks = props.decks.map((deck) =>
-      <>
-        <button className="list-group-item list-group-item-action" id={deck.name} onClick={()=>{console.log(deck.name)}}>
-          {deck.name}
-        </button>
-      </>
-    );
-    return <>
-      <div className="list-group" id="list-tab">
-        {listDecks}
-      </div>
-    </>
-  }
-
-
 
 export default CreateDeckPage;
-
-/*
-<ul className="list-group">
-            <li className="list-group-item active" aria-current="true" type="button" onClick={()=>{console.log("hej")}}>An active item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item active">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-          </ul>*/
