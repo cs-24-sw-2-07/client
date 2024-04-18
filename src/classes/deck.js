6909fe8494c7993ff8f8caba77901e661f41af3a
ChristianSteen-SW23 <144026639+ChristianSteen-SW23@users.noreply.github.com>
2024-04-18 14:25:01 +0200

    Card editor and deck builder (#19)
    * Need fix
    
    Co-authored-by: ninaBlegmand <ninaBlegmand@users.noreply.github.com>
    
    * Made page for deck selctor
    
    Co-authored-by: ninaBlegmand <ninaBlegmand@users.noreply.github.com>
    
    * Add card selction
    
    * Made UI and Buttons working with Nina
    
    * Tilføjet classes og pulled fra main med Nina
    
    * Use effect fixed with nina
    
    * lavet card name med nina
    
    * sortfunction better med nina
    
    * Made localStorge med nina
    
    * Everything should be done med Nina
    
    ---------
    
    Co-authored-by: ChristianSteen-SW23 <cniel@student.aau.dk>
    Co-authored-by: ninaBlegmand <ninaBlegmand@users.noreply.github.com>

--- src/classes/deck.js
+++ src/classes/deck.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 import { Card } from "./card.js";
 
 export class Deck {
-  constructor(name, id, cards) {
+  constructor({name, id, cards}) {
     this.name = name;
     if (id == null) {
       this.id = uuidv4(); //Uniqueness: https://stackoverflow.com/questions/1155008/how-unique-is-uuid
@@ -11,29 +11,9 @@ export class Deck {
     }
     if (cards == null) {
       this.cards = [];
-      this.cards.push(new Card("", ""));
+      this.cards.push(new Card({question:"", answer:"", name:""}));
     } else {
       this.cards = cards;
     }
-  }
-
-  addCard(question, answer, id) {
-    this.cards.push(new Card(question, answer, id));
-  }
-
-  removeCard(id) {
-    this.cards = this.cards.filter((card) => card.id !== id);
-  }
-
-  updateCard(question, answer, id) {
-    let cards = this.cards.filter((card) => card.id === id);
-    if (cards.length <= 0) {
-      return;
-    }
-    cards[0].updateCard(question, answer);
-  }
-
-  saveDeck() {
-    //localStorage.setItem...
   }
 }
