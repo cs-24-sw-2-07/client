4ed7bc408e27b83f088ac379859f3849fb2fdb8a
ChristianSteen-SW23 <144026639+ChristianSteen-SW23@users.noreply.github.com>
2024-04-25 11:07:13 +0200

    Update src/classes/deck.js
    Co-authored-by: Simon Stjernholm <simon@stjernholm.dev>

--- src/classes/deck.js
+++ src/classes/deck.js
@@ -3,17 +3,8 @@ import { Card } from "./card.js";
 
 export class Deck {
     constructor({name, id, cards}) {
-        this.name = name;
-        if (id == null) {
-            this.id = uuidv4(); //Uniqueness: https://stackoverflow.com/questions/1155008/how-unique-is-uuid
-        } else {
-            this.id = id;
-        }
-        if (cards == null) {
-            this.cards = [];
-            this.cards.push(new Card({question:"", answer:"", name:""}));
-        } else {
-            this.cards = cards;
-        }
+        this.name = name ?? "";
+        this.id = id ?? uuidv4(); //Uniqueness: https://stackoverflow.com/questions/1155008/how-unique-is-uuid
+        this.cards = cards ?? [new Card()]
     }
 }
