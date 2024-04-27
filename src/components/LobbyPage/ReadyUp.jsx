e246c635128689033b2580a88c050398c87b9f94
aske-design1 <hm64zk@student.aau.dk>
2024-04-27 20:59:13 +0200

    Added ready btn

--- src/components/LobbyPage/ReadyUp.jsx
+++ src/components/LobbyPage/ReadyUp.jsx
@@ -1,7 +1,18 @@
-export { ReadyUp }
+export { ReadyButton }
+import { socket } from "../../socket";
 
 
-
-function ReadyUp() {
-    
+function ReadyButton({ player, id }) {
+	return (
+		<div>
+            <button
+                type="button"
+                className="btn btn-primary col-4"
+				disabled={player.deck === null}
+                onClick={() => socket.emit("playerReady", id)}
+            >
+                Ready Up
+            </button>
+        </div>
+	);
 }
