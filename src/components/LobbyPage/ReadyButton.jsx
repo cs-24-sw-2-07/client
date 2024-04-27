44ef354d93f23255a193ace17ea66b514bc9a593
aske-design1 <hm64zk@student.aau.dk>
2024-04-27 20:54:48 +0200

    Final work on this

---
+++ src/components/LobbyPage/ReadyButton.jsx
@@ -0,0 +1,18 @@
+export { ReadyButton }
+import { socket } from "../../socket";
+
+
+function ReadyButton({ player, id }) {
+	return (
+		<div>
+            <button
+                type="button"
+                className="btn btn-primary col-4"
+				disabled={player.deck === null}
+                onClick={() => socket.emit("playerReady", id)}
+            >
+                Ready Up
+            </button>
+        </div>
+	);
+}
---
+++ src/components/LobbyPage/StartButton.jsx
@@ -0,0 +1,20 @@
+export { StartButton }
+import { socket } from "../../socket";
+
+function StartButton({ players, ready, id }) {
+    return (
+        <div>
+            <button
+                type="button"
+                className="btn btn-primary col-4"
+                disabled={players >= 2 && ready === players ? false : true}
+                onClick={() => socket.emit("StartGame", {id: id})}
+            >
+                Start game
+            </button>
+            <p className={ready === players ? "text-success" : "text-danger"}>
+                Players ready: {ready}/{players}
+            </p>
+        </div>
+    );
+}
