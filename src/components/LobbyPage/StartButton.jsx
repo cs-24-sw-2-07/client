0173ad38d52f123d249af51232756e1c186d0109
Aske-design1 <hm64zk@student.aau.dk>
2024-04-27 22:37:30 +0200

    looked at joined user settings

--- src/components/LobbyPage/StartButton.jsx
+++ src/components/LobbyPage/StartButton.jsx
@@ -2,18 +2,18 @@ export { StartButton }
 import { socket } from "../../socket";
 
 function StartButton({ players, ready, id }) {
-    return (
-        <div>
-            <button
-                type="button"
-                className="btn btn-primary col-4"
-                disabled={players < 2 || ready !== players}
-                onClick={() => socket.emit("StartGame", {id: id})}
-            > Start game
-            </button>
-            <p className={ready === players ? "text-success" : "text-danger"}>
+  return (
+    <div>
+      <button
+        type="button"
+        className="btn btn-primary col-4"
+        disabled={players < 2 || ready !== players}
+        onClick={() => socket.emit("StartGame", {id: id})}
+      > Start game
+      </button>
+      <p className={ready === players ? "text-success" : "text-danger"}>
                 Players ready: {ready}/{players}
-            </p>
-        </div>
-    );
+      </p>
+    </div>
+  );
 }
