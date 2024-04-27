48f67d6168a81db453c4e5139b425e0c00eb4141
Aske-design1 <hm64zk@student.aau.dk>
2024-04-27 22:36:42 +0200

    looked at joined user settings

--- src/components/LobbyPage/JoinedSettings.jsx
+++ src/components/LobbyPage/JoinedSettings.jsx
@@ -5,65 +5,30 @@ export { JoinedSettings }
 function JoinedSettings({ lobbyState }) {
   //Setting states:
   const [settingsState, setSettingsState] = useState({
-    cardCount: lobbyState.deckSize,
+    deckSize: lobbyState.deckSize,
     handSize: lobbyState.handSize,
     maxLife: lobbyState.life,
     lobbySize: lobbyState.lobbySize,
   });
-    
+
+  //TODO: Implement event here 
+
   return (
     <div>
       <h2>Settings</h2>
-      <form>
-        <div className="form-group">
-          <div className="col-4 bg-light text-dark">
-            <label htmlFor="decksize">Deck Size:</label>
-            <input
-              type="number"
-              className="form-control"
-              id="decksize"
-              value={settingsState.cardCount}
-              onChange={(e) => setSettingsState({
-                ...settingsState,
-                cardCount: e.target.value
-              })}
-            ></input>
-            <label htmlFor="handsize"> Hand Size:</label>
-            <input
-              type="number"
-              className="form-control"
-              id="handsize"
-              value={settingsState.handSize}
-              onChange={(e) => setSettingsState({
-                ...settingsState,
-                handSize: e.target.value
-              })}
-            ></input>
-            <label htmlFor="Maxlife"> Life: </label>
-            <input
-              type="number"
-              className="form-control"
-              id="lifesize"
-              value={settingsState.maxLife}
-              onChange={(e) => setSettingsState({
-                ...settingsState,
-                maxLife: e.target.value
-              })}
-            ></input>
-            <label htmlFor="lobbySize"> Lobby Size:</label>
-            <input
-              type="number"
-              className="form-control"
-              id="lobbySize"
-              value={settingsState.lobbySize}
-              onChange={(e) => setSettingsState({
-                ...settingsState,
-                LobbySize: e.target.value
-              })}
-            ></input>
-          </div>
-        </div>
-      </form>
+      <ShowSetting label="Deck Size:" setting={settingsState.deckSize}/>
+      <ShowSetting label="Hand Size:" setting={settingsState.handSize}/>
+      <ShowSetting label="Life:" setting={settingsState.life}/>
+      <ShowSetting label="lobbySize:" setting={settingsState.lobbySize}/>
+    </div>
+  );
+}
+
+function ShowSetting(label, setting) {
+  return (
+    <div className="row">
+      <div className="col"><p>{label}</p></div>
+      <div className="col text-end"><p>{setting}</p></div>
     </div>
   );
 }
