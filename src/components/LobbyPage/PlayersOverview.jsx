a44c46c133b84ceb8a4ef6e5bcc020cc1ded0e05
Aske-design1 <hm64zk@student.aau.dk>
2024-04-27 20:06:06 +0200

    merged test branch

---
+++ src/components/LobbyPage/PlayersOverview.jsx
@@ -0,0 +1,30 @@
+import { socket } from "../../socket";
+import { useState, useEffect } from "react";
+import { } from "react-router-dom";
+export { PlayerOverview };
+
+function PlayerOverview({ playersArr }) {
+  /*playersArr[0] = {
+		name: value.name,
+		ready: value.ready,
+		host: value.host,
+		playerid: key
+	}
+	*/
+  return (
+    <>
+      <h2>Players: </h2>
+      {playersArr.map(player => PrintPlayer(player))}
+    </>
+  );
+}
+
+function PrintPlayer(player) {
+  const isYou = player.playerid === socket.id; 
+
+  return (
+    <p>
+      {player.name} {isYou ? "(You)" : ""} 
+    </p>
+  );
+}
---
+++ src/components/LobbyPage/StartButton.jsx
@@ -0,0 +1,27 @@
+export { StartButton }
+import { socket } from "../../socket";
+
+function StartButton({ players, ready }) {
+  return (
+    <div>
+      <button
+        type="button"
+        className="btn btn-primary col-4"
+        disabled={players >= 2 && ready === players ? false : true}
+        onClick={() => StartGame(players, ready)}
+      >
+                Start game
+      </button>
+      <p className={ready === players ? "text-success" : "text-danger"}>
+                Players ready: {ready}/{players}
+      </p>
+    </div>
+  );
+}
+
+//TODO: Ponder whether the button should check if people are ready or an event should --> Event would probably make more sense
+function StartGame() {
+  //TODO: Start game event here
+  //Make an obj that contains the room id
+  socket.emit("StartGame");
+}
