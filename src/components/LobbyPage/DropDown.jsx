0e8b9592fed008a356973b3d82b14e3ce0c51087
Aske-design1 <hm64zk@student.aau.dk>
2024-04-29 09:07:13 +0200

    From weekend

--- src/components/LobbyPage/DropDown.jsx
+++ src/components/LobbyPage/DropDown.jsx
@@ -1,69 +1,72 @@
 import { socket } from "../../socket";
-import { useState, useEffect } from "react";
 import { } from "react-router-dom";
 
 export { DeckDropDown };
 
-function DeckDropDown( { id }) {
-    return (
-        <div className="dropdown">
-            <div className="btn-group">
-                <button
-                    className="btn btn-primary dropdown-toggle"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                >
-                    Choose Deck
-                </button>
-                <GetDecksDropDown id={id} />
-            </div>
-        </div>
-    );
+function DeckDropDown({ id }) {
+  return (
+    <div className="dropdown">
+      <div className="btn-group">
+        <button
+          className="btn btn-primary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        > Choose Deck
+        </button>
+        <GetDecksDropDown id={id} />
+      </div>
+    </div>
+  );
 }
 //onClick={() => socket.emit("test", "/12345 ")
-function GetDecksDropDown( { id }) {
-    const decks = JSON.parse(localStorage.getItem("userDeck")); //Check spelling
-    if (decks === null) {
-        return (
-            <ul className="dropdown-menu">
-                <li>
-                    <button type="button" className="dropdown-item">
-                        No decks to choose from :/
-                    </button>
-                </li>
-            </ul>
-        );
-    }
-    /**/
-    //Creates an option for every deck saved in localStorage
+function GetDecksDropDown({ id }) {
+  const decks = JSON.parse(localStorage.getItem("userDeck")); //Check spelling
+  if (decks === null) {
     return (
-        <ul className="dropdown-menu">
-            {decks.map((deck, index) => {
-                return (
-                <li>
-                    <button
-                        type="button"
-                        className="dropdown-item"
-                        key={index}
-                        onClick={() => addDeck(deck, id)}>
-                        {deck.name}
-                    </button>
-                </li>);
-            })}
-        </ul>
+      <ul className="dropdown-menu">
+        <li>
+          <button type="button" className="dropdown-item">
+            No decks to choose from :/
+          </button>
+        </li>
+      </ul>
     );
+  }
+  /**/
+  //Creates an option for every deck saved in localStorage
+  return (
+    <ul className="dropdown-menu">
+      {decks.map((deck, index) => (
+        <li key={index}>
+          <button
+            type="button"
+            className="dropdown-item"
+            onClick={() => addDeck(deck, id)}
+          >{deck.name}
+          </button>
+        </li>))
+      }
+    </ul>
+  );
 }
 
 function addDeck(deck, roomID) {
-    // Add room id from the server
-    /*const data = {
+  // Add room id from the server
+  /*const data = {
       deck: deck, 
       id: lobbyIgitd, 
     }*/
-    const eventObj = {
-        deck: deck, 
-        id: roomID
-    };
-    socket.emit("chooseDeck", eventObj);
+  const eventObj = {
+    deck: deck,
+    id: roomID
+  };
+  socket.emit("chooseDeck", eventObj);
 }
+
+
+/**
+//TODO: Ting der mangler
+ * Få client til at kommunikere med server
+ * Lave playerOverview, så den er scrollable
+ */
