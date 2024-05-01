d8e8bc77d2ee68ce59307b3c3108ac9c59453e21
Simon Stjernholm <simon@stjernholm.dev>
2024-05-01 15:41:17 +0200

    fixed lint

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    port: 8081,
-    strictPort: true,
-  }
+    plugins: [react()],
+    preview: {
+        port: 8081,
+        strictPort: true,
+    }
 })
