03cdd26197884708ae1e8d3755a1d8870767363b
Simon Stjernholm <simon@stjernholm.dev>
2024-05-06 14:50:17 +0200

    Fixed classes (#32)
    Co-authored-by: ChristianSteen-SW23 <cniel@student.aau.dk>

--- src/classes/deck.js
+++ src/classes/deck.js
@@ -1,10 +1,9 @@
-import { v4 as uuidv4 } from "uuid";
 import { Card } from "./card.js";
 
 export class Deck {
-    constructor({name, id, cards}) {
+    constructor(values) {
+        const {name, cards} = values || {};
         this.name = name ?? "";
-        this.id = id ?? uuidv4(); //Uniqueness: https://stackoverflow.com/questions/1155008/how-unique-is-uuid
         this.cards = cards ?? [new Card()]
     }
 }
