9406ecf974d640ea36778f225f93dbaa5b55e7a8
ChristianSteen-SW23 <cniel@student.aau.dk>
2024-05-06 14:21:37 +0200

    Fixed classes

--- src/classes/card.js
+++ src/classes/card.js
@@ -1,9 +1,6 @@
-import { v4 as uuidv4 } from "uuid";
-
 export class Card {
     constructor(values) {
-        const {question, answer, id, name} = values || {};
-        this.id = id ?? uuidv4(); //Uniqueness: https://stackoverflow.com/questions/1155008/how-unique-is-uuid
+        const {question, answer, name} = values || {};
         this.answer = answer ?? "";
         this.question = question ?? "";
         this.name = name ?? "";
