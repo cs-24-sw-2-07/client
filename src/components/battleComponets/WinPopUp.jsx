0498ae8444b138948ad20068511baab7da74a69a
ChristianSteen-SW23 <cniel@student.aau.dk>
2024-05-13 11:37:33 +0200

    added localstorge intragration collba with Zafir

--- src/components/battleComponets/WinPopUp.jsx
+++ src/components/battleComponets/WinPopUp.jsx
@@ -1,11 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { Deck } from "./../../classes/deck.js"
-import { Card } from "./../../classes/card.js"
+//import { Card } from "./../../classes/card.js"
 
 function WinPopUp(props) {
     const navigate = useNavigate();
     const [saveCards, setSaveCards] = useState([]);
+    
+    function saveAndReturn() {
+        const oldDecks = JSON.parse(localStorage.getItem("userDeck"));
+        const FeedbackDeck = new Deck({name : props.feedbackDeck.name+" feedback", cards: saveCards });
+        const allDecks = [...oldDecks,FeedbackDeck]
+        localStorage.setItem("userDeck", JSON.stringify(allDecks));
+
+        navigate("/");
+    }
 
     return (
         <>
@@ -17,7 +26,8 @@ function WinPopUp(props) {
                         </div>
                         <div className='modal-body'>
                             {/* Feedback code */}
-                            <h6 className="text-primary"> Choose cards to add to feedback deck</h6>
+                            <h6 className="text-primary"> Pick incorrectly answerd cards to add to feedback deck with name: <span className="text-danger"> {props.feedbackDeck.name+" feedback"}</span></h6>
+                            
                             <ListFeedback
                                 saveCards={saveCards}
                                 setSaveCards={setSaveCards}
@@ -27,7 +37,7 @@ function WinPopUp(props) {
                         </div>
                         <div className='modal-footer'>
                             <button type="button" className="btn btn-danger" data-bs-dismiss="modal"
-                                onClick={() => { navigate("/"); }}>Go to front page</button>
+                                onClick={saveAndReturn}>Go to front page</button>
                         </div>
                     </div>
                 </div>
@@ -42,29 +52,28 @@ export { WinPopUp };
 
 
 function ListFeedback(props) {
-    function addCardToFeedback(index){
+    function addCardToFeedback(index) {
         console.log(index)
         let tempSaveCards = props.saveCards;
         tempSaveCards.push(props.feedbackDeck.cards[index]);
 
         props.setSaveCards(tempSaveCards)
-        let tempFeedbackDeck = {...props.feedbackDeck};
+        let tempFeedbackDeck = { ...props.feedbackDeck };
         tempFeedbackDeck.cards.splice(index, 1)
-        console.log(tempFeedbackDeck)
         props.setFeedbackDeck(tempFeedbackDeck)
     }
-    
-    
+
+
     return <div className="list-group" id="list-tab" key="list">
         {props.feedbackDeck.cards.map((card, index) =>
             <div className="row" key={index}>
                 <div className="col-9">
-                    <button type="button" className="list-group-item list-group-item-action" onClick={() => { }}>
+                    <p className="list-group-item">
                         {card.name}
-                    </button>
+                    </p>
                 </div>
                 <div className="col-3">
-                    <button type="button" className="list-group-item list-group-item-action text-center" onClick={()=>{addCardToFeedback(index)}}>
+                    <button type="button" className="list-group-item list-group-item-action text-center" onClick={() => { addCardToFeedback(index) }}>
                         Save
                     </button>
                 </div>
