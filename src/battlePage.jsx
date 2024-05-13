4e7840c8b267c906a86b8ac58a4c406a5600ac62
ChristianSteen-SW23 <cniel@student.aau.dk>
2024-05-13 10:12:52 +0200

    Added UI for feedback med Zafir

--- src/battlePage.jsx
+++ src/battlePage.jsx
@@ -5,6 +5,8 @@ import { WinPopUp } from "./components/battleComponets/WinPopUp.jsx";
 import { DisplayButtons } from "./components/battleComponets/DisplayButtons.jsx";
 import { socket } from "./socket.js"
 
+//import { Deck } from "./classes/deck.js"
+
 function BattlePage(props) {
     const myDeck = useRef(props.data);
     const [hand, setHand] = useState([]);
@@ -16,6 +18,9 @@ function BattlePage(props) {
     const [showWonPopUp, setShowWonPopUp] = useState(false);
     const [gameResult, setGameResult] = useState("");
     const [showAnswer, setShowAnswer] = useState(false);
+    const [feedbackDeck, setFeedbackDeck] = useState({"name":"Programming","id":"c197aab2-55ab-4724-9932-ff0d3638a5b7","cards":[{"id":"6a658d2f-7cc7-439b-b031-efe228e57c4f","answer":"test1","question":"test1","name":"test1"},{"id":"5982685b-3a96-4e0e-829e-d358d484f88d","answer":"test2","question":"test2","name":"test2"},{"id":"088c44d2-165c-4161-ab81-ee23dd9c7224","answer":"test3","question":"test3","name":"test3"},{"id":"ed39caa2-e563-4a74-ac07-25f3eb442823","answer":"test4","question":"test4","name":"test4asdsad"},{"id":"c130187c-3951-4c1d-9549-69e26204f5f9","answer":"test5","question":"test5","name":"test5"},{"id":"00b32047-0a5c-48a2-b33f-28662e5f2f7e","answer":"test6","question":"test6","name":"test6"},{"id":"4f52105a-63ce-4c45-943d-b34acf11b651","answer":"test7","question":"test7","name":"test7"},{"id":"d43f609e-4299-4b2e-8fc0-0d0641219c2a","answer":"test8","question":"test8","name":"test8"},{"id":"ff745cf4-e004-4c47-869c-3ce75351dddd","answer":"test9","question":"test9","name":"test9"},{"id":"895134f9-0acb-4507-a2f6-28632c82bc62","answer":"test10","question":"test10","name":"test10"}]});
+
+    
 
     useEffect(() => {
         if (turnRef.current.current !== socket.id) setdisableCards(true);
@@ -98,6 +103,7 @@ function BattlePage(props) {
             <WinPopUp
                 foundWinner={showWonPopUp}
                 gameResult={gameResult}
+                feedbackDeck={feedbackDeck}
             />
 
             {/* Displays the card that are played */}
--- src/components/battleComponets/WinPopUp.jsx
+++ src/components/battleComponets/WinPopUp.jsx
@@ -1,19 +1,27 @@
 import { useNavigate } from "react-router-dom";
 
-function WinPopUp(props){
+function WinPopUp(props) {
     const navigate = useNavigate();
-    return(
+    return (
         <>
-            <div className={`modal fade ${(props.foundWinner == true) ? "show" : ""}`} id={"nameModal"} aria-hidden="true" data-bs-backdrop="static" aria-labelledby="staticBackdropLabel" style={{display: (props.foundWinner == true) ? "block" : "none"}}>
+            <div className={`modal fade ${(props.foundWinner == true) ? "show" : ""}`} id={"nameModal"} aria-hidden="true" data-bs-backdrop="static" aria-labelledby="staticBackdropLabel" style={{ display: (props.foundWinner == true) ? "block" : "none" }}>
                 <div className='modal-dialog'>
                     <div className='modal-content'>
                         <div className='modal-header'>
                             <h1>You {props.gameResult}</h1>
                         </div>
-                        {/* ! Add a body for feedback here */}
+                        <div className='modal-body'>
+                            {/* Feedback code */}
+                            <p> Choose cards to add to feedback deck</p>
+                            <ListFeedback
+                                feedbackDeck={props.feedbackDeck}
+                            />
+                            
+                            
+                        </div>
                         <div className='modal-footer'>
-                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" 
-                                onClick={() => {navigate("/");}}>Go to front page</button>
+                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal"
+                                onClick={() => { navigate("/"); }}>Go to front page</button>
                         </div>
                     </div>
                 </div>
@@ -25,3 +33,25 @@ function WinPopUp(props){
     )
 }
 export { WinPopUp };
+
+
+function ListFeedback(props) {
+
+    
+    return <div className="list-group" id="list-tab" key="list">
+        {props.feedbackDeck.cards.map((card, index) =>
+            <div className="row" key={index}>
+                <div className="col-9">
+                    <button type="button" className="list-group-item list-group-item-action" onClick={() => { }}>
+                        {card.name}
+                    </button>
+                </div>
+                <div className="col-3">
+                    <button type="button" className="list-group-item list-group-item-action text-center" onClick={() => {  }}>
+                        Save
+                    </button>
+                </div>
+            </div>)
+        }
+    </div>
+}
